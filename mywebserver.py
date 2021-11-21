561aa47ec3b226f7917dfa557a7dbd2a33ff3602
RanjithD18 <ranjith182003@gmail.com>
2021-11-21 05:47:24 +0530

    https://github.com/RanjithD18/Web_server.git

--- mywebserver.py
+++ mywebserver.py
@@ -1,31 +1,9 @@
-# Developing a Simple Webserver
-## AIM:
-
-To develop a simple webserver to serve html pages.
-## DESIGN STEPS:
-### Step 1:
-
-HTML content creation
-### Step 2:
-
-Design of webserver workflow
-### Step 3:
-
-Implementation using Python code
-### Step 4:
-
-Serving the HTML pages.
-### Step 5:
-
-Testing the webserver
-## PROGRAM:
-~~~
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
 <html>
 <head>
-<title>My webserver</title>
+<title>Mywebserver</title>
 </head>
 <body>
 <h1>Ranjith D</h1>
@@ -45,6 +23,3 @@ server_address = ('',8080)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-~~~
-## OUTPUT:
-## RESULT:
